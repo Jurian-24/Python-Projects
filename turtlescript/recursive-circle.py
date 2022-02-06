eb4fd60d03fdad1b131987e3674198c83f68b56e
Jurian-24 <jurian.andriessen@gmail.com>
2022-02-06 18:53:54 +0100

    chore(feat): Added a recursive function that draws a circle

---
+++ turtlescript/recursive-circle.py
@@ -0,0 +1,206 @@
+import turtle
+import random
+
+colors = [
+  "AliceBlue",
+  "AntiqueWhite",
+  "Aqua",
+  "Aquamarine",
+  "Azure",
+  "Beige",
+  "Bisque",
+  "Black",
+  "BlanchedAlmond",
+  "Blue",
+  "BlueViolet",
+  "Brown",
+  "BurlyWood",
+  "CadetBlue",
+  "Chartreuse",
+  "Chocolate",
+  "Coral",
+  "CornflowerBlue",
+  "Cornsilk",
+  "Crimson",
+  "Cyan",
+  "DarkBlue",
+  "DarkCyan",
+  "DarkGoldenRod",
+  "DarkGray",
+  "DarkGrey",
+  "DarkGreen",
+  "DarkKhaki",
+  "DarkMagenta",
+  "DarkOliveGreen",
+  "DarkOrange",
+  "DarkOrchid",
+  "DarkRed",
+  "DarkSalmon",
+  "DarkSeaGreen",
+  "DarkSlateBlue",
+  "DarkSlateGray",
+  "DarkSlateGrey",
+  "DarkTurquoise",
+  "DarkViolet",
+  "DeepPink",
+  "DeepSkyBlue",
+  "DimGray",
+  "DimGrey",
+  "DodgerBlue",
+  "FireBrick",
+  "FloralWhite",
+  "ForestGreen",
+  "Fuchsia",
+  "Gainsboro",
+  "GhostWhite",
+  "Gold",
+  "GoldenRod",
+  "Gray",
+  "Grey",
+  "Green",
+  "GreenYellow",
+  "HoneyDew",
+  "HotPink",
+  "IndianRed",
+  "Indigo",
+  "Ivory",
+  "Khaki",
+  "Lavender",
+  "LavenderBlush",
+  "LawnGreen",
+  "LemonChiffon",
+  "LightBlue",
+  "LightCoral",
+  "LightCyan",
+  "LightGoldenRodYellow",
+  "LightGray",
+  "LightGrey",
+  "LightGreen",
+  "LightPink",
+  "LightSalmon",
+  "LightSeaGreen",
+  "LightSkyBlue",
+  "LightSlateGray",
+  "LightSlateGrey",
+  "LightSteelBlue",
+  "LightYellow",
+  "Lime",
+  "LimeGreen",
+  "Linen",
+  "Magenta",
+  "Maroon",
+  "MediumAquaMarine",
+  "MediumBlue",
+  "MediumOrchid",
+  "MediumPurple",
+  "MediumSeaGreen",
+  "MediumSlateBlue",
+  "MediumSpringGreen",
+  "MediumTurquoise",
+  "MediumVioletRed",
+  "MidnightBlue",
+  "MintCream",
+  "MistyRose",
+  "Moccasin",
+  "NavajoWhite",
+  "Navy",
+  "OldLace",
+  "Olive",
+  "OliveDrab",
+  "Orange",
+  "OrangeRed",
+  "Orchid",
+  "PaleGoldenRod",
+  "PaleGreen",
+  "PaleTurquoise",
+  "PaleVioletRed",
+  "PapayaWhip",
+  "PeachPuff",
+  "Peru",
+  "Pink",
+  "Plum",
+  "PowderBlue",
+  "Purple",
+  "Red",
+  "RosyBrown",
+  "RoyalBlue",
+  "SaddleBrown",
+  "Salmon",
+  "SandyBrown",
+  "SeaGreen",
+  "SeaShell",
+  "Sienna",
+  "Silver",
+  "SkyBlue",
+  "SlateBlue",
+  "SlateGray",
+  "SlateGrey",
+  "Snow",
+  "SpringGreen",
+  "SteelBlue",
+  "Tan",
+  "Teal",
+  "Thistle",
+  "Tomato",
+  "Turquoise",
+  "Violet",
+  "Wheat",
+  "White",
+  "WhiteSmoke",
+  "Yellow",
+  "YellowGreen",
+]
+
+window = turtle.Screen()
+jurian = turtle.Turtle()
+jurian.speed(0)
+jurian.width(2)
+jurian.pencolor(random.choice(colors))
+window.bgcolor('black')
+
+# def shape(angle, side, limit):
+#     reverseDirection = 300
+#     jurian.forward(side)
+
+#     if side % (reverseDirection*2) == 0:
+#         angle = angle + 2
+#         print(side)
+#     elif side % reverseDirection == 0:
+#         angle = angle - 2
+#         print(side)
+    
+#     jurian.right(angle)
+#     side = side + 2
+#     if side < limit:
+#         shape(angle, side, limit)
+    
+# angle = 119
+# side = 90
+# limit = 1000
+# shape(angle, side, limit)
+
+def circle(diameter, angle, limit):
+    reverseDirection = 300
+    jurian.forward(diameter)
+    diameter = diameter - 1
+    if diameter % (reverseDirection*2) == 0:
+        angle = angle - 2
+    elif diameter % reverseDirection == 0:
+        angle = angle + 2
+
+    jurian.right(angle)
+    diameter = diameter + 4
+    jurian.circle(diameter)
+    
+    circle(diameter, angle, limit)
+
+
+
+
+diameter = 100
+angle = 180
+limit = 200
+circle(diameter, angle, limit)
+
+
+turtle.done()
